9e2dd8b352c388a31b3712217de00378240cf7cf
abdurahimov0101 <abrorjonabdurahimov42@gmail.com>
2024-07-26 20:43:20 +0300

    6-dars

---
+++ js/index.js
@@ -0,0 +1,96 @@
+// 1-masala
+// let arr = [3, 7, 4, 5, 6];
+// let largest = -Infinity;
+// let second = -Infinity;
+
+// for (let num of arr) {
+//     if (num > largest) {
+//         second = largest;
+//         largest = num;
+//     } else if (num > second && num < largest) {
+//         second = num;
+//     }
+// }
+
+// console.log(second); 
+
+// 2-masala
+
+
+// let arr = ["olma", "banan", "wkiwi"];
+// let lengths = [];
+
+// for (let str of arr) {
+//     lengths.push(str.length);
+// }
+
+// console.log(lengths); 
+
+// 3-masala
+
+// let arr = [3, 7, 4, 5, 6];
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0 && i < arr.length - 1) {
+//         arr[i] = arr[i + 1];
+//     }
+// }
+
+// console.log(arr); 
+// 4-masala
+
+// let arr = [3, 7, 4, 5, 6];
+// let reversed = arr.reverse();
+
+// for (let i = 0; i < reversed.length; i++) {
+//     if (reversed[i] % 2 !== 0) {
+//         reversed[i] = 0;
+//     }
+// }
+
+// console.log(reversed);
+
+// 5-masala
+
+// let arr = [3, 7, 4, 5, 6];
+// let newArr = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//     newArr.push(i);
+// }
+
+// console.log(newArr); 
+
+// 6-masala
+
+// let arr = [3, 7, 4, 5, 6, 9];
+// let result = [];
+
+// for (let num of arr) {
+//     if (num % 3 !== 0) {
+//         result.push(num);
+//     }
+// }
+
+// console.log(result); 
+
+// 8-masala
+
+// let arr = [1, 2, 3, 4, 5];
+// for (let i = arr.length - 1; i > 0; i--) {
+//     arr[i] = arr[i - 1];
+// }
+// console.log(arr);
+
+// 9-masala
+
+// let arr = [1, -2, 3, -4, 5];
+// let positiveArr = arr.filter(num => num >= 0);
+// console.log(positiveArr.length);
+
+
+
+
+
+
+
